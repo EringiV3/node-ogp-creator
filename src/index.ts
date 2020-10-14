2c07cf793603d304c1c0d5d11c3796dab889f30c
萩原孝大 <kodaihagihara@EringiV3.local>
2020-10-15 00:38:09 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,84 @@
-import { createCanvas, registerFont, CanvasRenderingContext2D } from "canvas";
+import { createCanvas, CanvasRenderingContext2D, Canvas } from "canvas";
 import fs from "fs-extra";
 import { options } from "./types/index";
 
-// あとでheight, widthはcondigから受け取るようにする
-const canvas = createCanvas(640, 480);
-const ctx = canvas.getContext("2d");
-
-function renderToCanvas(ctx: CanvasRenderingContext2D, title: string) {
-  ctx.font = "30px 'sans-serif'";
-  ctx.textAlign = "left";
-  ctx.textBaseline = "top";
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, 640, 480);
-  ctx.fillStyle = "black";
-  ctx.fillText(title, 320, 240);
+function renderToCanvas(ctx: CanvasRenderingContext2D, options: options) {
+  ctx.font = `${options.styles.fontSize}px ${
+    options.styles.font || "sans-serif"
+  }`;
+  ctx.fillStyle = options.styles.backgroudColor || "#fff";
+  ctx.fillRect(0, 0, options.width, options.height);
+  ctx.fillStyle = options.styles.textColor || "black";
+  const [x, y] = getTitlePosition(options.width, options.height, options.title);
+  ctx.fillText(options.title, x, y);
+  ctx.fillText(
+    `@${options.userName}`,
+    options.styles.userNamePositionX || 450,
+    options.styles.userNamePositionY || 400
+  );
 }
 
-function exportFile(path: string) {
+// TODO implement
+function getTitlePosition(
+  width: number,
+  height: number,
+  title: string
+): number[] {
+  return [320, 240];
+}
+
+function exportFile(canvas: Canvas, path: string) {
   const body: string = canvas.toDataURL();
   const base64Data: string = body.replace(/^data:image\/png;base64,/, "");
   const binaryData: string = Buffer.from(base64Data, "base64").toString(
     "binary"
   );
-
   fs.mkdirSync(path.split("/").slice(0, -1).join("/"), { recursive: true });
-  fs.writeFile(path, binaryData, "binary", function (err: any) {
+  fs.writeFile(path, binaryData, "binary", (err: any) => {
     console.log(err);
   });
 }
 
-function main(options: options) {
-  const { title, path, userName } = options;
-  renderToCanvas(ctx, title);
-  exportFile(path);
-}
+// あとでheight, widthはcondigから受け取るようにする
+(() => {
+  const width = 640;
+  const height = 480;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
+  renderToCanvas(ctx, {
+    title: "title_example_hogea",
+    userName: "eringiV3",
+    path: "public/ogp/hoge.png",
+    width,
+    height,
+    styles: {
+      backgroudColor: "#fff",
+      textColor: "black",
+      font: "sans-serif",
+      fontSize: "30px",
+      userNamePositionX: 450,
+      userNamePositionY: 400,
+    },
+  });
+  exportFile(canvas, "public/ogp/hoge.png");
+})();
 
-main({
-  title: "title_example",
-  path: "public/ogp/hoge.png",
-  userName: "eringiV3",
-});
+/**
+ * export const createOGP = (options) => {
+ *    const canvas = createCanvas(width, height);
+ *    const ctx = canvas.getContext("2d");
+ *    renderToCanvas(ctx, options);
+ *    exportFile(canvas, options.path);
+ * }
+ */
+
+/**
+ * usage
+ *
+ * import { createOGP } from 'node-ogp-creator';
+ * createOGP({
+ *     title: "title_example_hogea",
+ *     userName: "eringiV3",
+ *     path: "public/ogp/hoge.png",
+ * })
+ */
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,16 @@ export type options = {
   title: string;
   path: string;
   userName: string;
-  width?: number;
-  height?: number;
-  font?: string;
-  backgroudColor?: string;
+  width: number;
+  height: number;
+  styles: {
+    width?: number;
+    height?: number;
+    font?: string;
+    backgroudColor?: string;
+    textColor?: string;
+    fontSize?: string;
+    userNamePositionX?: number;
+    userNamePositionY?: number;
+  };
 };
