52ddbbe3842a438faa24b3242be779b1e9d15650
萩原孝大 <kodaihagihara@EringiV3.local>
2020-10-14 23:48:51 +0900

    画像出力まで

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+import { createCanvas, registerFont, CanvasRenderingContext2D } from "canvas";
+import fs from "fs-extra";
+import { options } from "./types/index";
+
+// あとでheight, widthはcondigから受け取るようにする
+const canvas = createCanvas(640, 480);
+const ctx = canvas.getContext("2d");
+
+function renderToCanvas(ctx: CanvasRenderingContext2D, title: string) {
+  ctx.font = "30px 'sans-serif'";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 640, 480);
+  ctx.fillStyle = "black";
+  ctx.fillText(title, 320, 240);
 }
 
-console.log(hello("TypeScript"));
+function exportFile(path: string) {
+  const body: string = canvas.toDataURL();
+  const base64Data: string = body.replace(/^data:image\/png;base64,/, "");
+  const binaryData: string = Buffer.from(base64Data, "base64").toString(
+    "binary"
+  );
+
+  fs.mkdirSync(path.split("/").slice(0, -1).join("/"), { recursive: true });
+  fs.writeFile(path, binaryData, "binary", function (err: any) {
+    console.log(err);
+  });
+}
+
+function main(options: options) {
+  const { title, path, userName } = options;
+  renderToCanvas(ctx, title);
+  exportFile(path);
+}
+
+main({
+  title: "title_example",
+  path: "public/ogp/hoge.png",
+  userName: "eringiV3",
+});
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type options = {
+  title: string;
+  path: string;
+  userName: string;
+  width?: number;
+  height?: number;
+  font?: string;
+  backgroudColor?: string;
+};
